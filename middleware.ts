2202ba3846db5445d63be0a2634bb9c91a232987
Dhrutii <thakkardhruti22@gmail.com>
2025-05-07 15:48:29 +0530

    role based access

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,61 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import {
+  clerkClient,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook/register",
+  "/sign-up",
+  "/sign-in",
+]);
+
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { userId } = await auth();
+  const pathname = req.nextUrl.pathname;
+
+  if (!userId && !isPublicRoute(req)) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+
+  const client = await clerkClient();
+
+  if (userId) {
+    try {
+      const user = await client.users.getUser(userId);
+      const role = user.publicMetadata.role as string | undefined;
+
+      // Admin user trying to go to /dashboard
+      if (role === "admin" && pathname === "/dashboard") {
+        return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+      }
+
+      // Non-admin user trying to go to /admin/*
+      if (role !== "admin" && pathname.startsWith("/admin")) {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+
+      // Authenticated users trying to access public routes
+      if (isPublicRoute(req)) {
+        return NextResponse.redirect(
+          new URL(role === "admin" ? "/admin/dashboard" : "/dashboard", req.url)
+        );
+      }
+    } catch (error) {
+      console.error("Error while fetching user data from Clerk:", error);
+      return NextResponse.redirect(new URL("/error", req.url));
+    }
+    return NextResponse.next();
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
