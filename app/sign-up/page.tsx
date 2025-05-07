e4f819b7b21f8d83e4d9f7f028388dbb0b9c195d
Dhrutii <thakkardhruti22@gmail.com>
2025-05-07 15:49:13 +0530

    sign-up page

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -16,7 +16,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Alert, AlertDescription } from "@/components/ui/alert";
 
 import { Label } from "@/components/ui/label";
 import { Eye, EyeOff } from "lucide-react";
